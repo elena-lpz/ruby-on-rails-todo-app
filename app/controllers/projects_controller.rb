0bfbb8b2206c6a25bc0f89988d448d4d85a3a149
elena-lpz <emloppal@gmail.com>
2025-08-29 11:38:10 +0100

    feat: add create project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,4 +10,13 @@ class ProjectsController < ApplicationController
     def new 
         @project = Project.new
     end
+
+    def create
+    @project = Project.new(project_params)
+
+    if @project.save
+        redirect_to project_path(@project)
+    else
+        render :new
+    end
 end
